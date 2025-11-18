975073639d828cc4d897799e6dbee4673500532e
elcoosp <elcoosp@gmail.com>
2025-11-18 09:57:16 +0100

    feat: if bad request send self as code & message

--- locolized-errors/src/lib.rs
+++ locolized-errors/src/lib.rs
@@ -2,15 +2,22 @@ use loco_rs::prelude::*;
 
 // Re-export the derive macro
 pub use locolized_errors_macros::LocalizedApiError;
+use serde::Serialize;
 
 // Main error trait
-pub trait LocalizedApiError: std::fmt::Display + Send + Sync + 'static {
+pub trait LocalizedApiError: std::fmt::Display + Send + Sync + 'static + Serialize {
     fn error_kind(&self) -> ErrorKind;
 
     fn to_loco_error(&self) -> Error {
         let message = self.to_string();
         match self.error_kind() {
-            ErrorKind::BadRequest => Error::BadRequest(message),
+            ErrorKind::BadRequest => {
+                let encoded = serde_json::json!({
+                    "code": self,
+                    "message": message
+                });
+                Error::BadRequest(serde_json::to_string(&encoded).unwrap())
+            }
             ErrorKind::Unauthorized => Error::Unauthorized(message),
             ErrorKind::NotFound => Error::NotFound,
             ErrorKind::InternalServerError => Error::InternalServerError,
